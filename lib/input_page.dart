3f2fb70747c13d1d2efe5e7921a363675f98d384
R12 <adomingb@gmail.com>
2023-03-09 22:20:27 +0100

    Widgets Themes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -84,17 +84,27 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                    value: height.toDouble(),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height = newValue.toInt();
-                      });
-                    },
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    min: 120.0,
-                    max: 220,
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x40EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.toInt();
+                        });
+                      },
+                      min: 120.0,
+                      max: 220,
+                    ),
                   )
                 ],
               ),
@@ -104,13 +114,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                    child: Container(
-                  color: kActiveCardColor,
-                )),
+                  child: ReusableCard(
+                    cardFunction: () {},
+                    colour: kActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [],
+                    ),
+                  ),
+                ),
                 Expanded(
-                    child: Container(
-                  color: kActiveCardColor,
-                )),
+                  child: ReusableCard(
+                    cardFunction: () {},
+                    colour: kActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
